7bff4ab7c75a81461b8bcfefa681065e108006cf
sproods <106177602+sproods@users.noreply.github.com>
2023-02-08 21:19:12 -0500

    Creación de la función que recoge la ficha de cada jugador.

--- tresEnRaya.c
+++ tresEnRaya.c
@@ -6,6 +6,7 @@
 
 void loop(char c[3][3]);            // Prototipos de funciones.
 void Intro_Primera(char c[3][3]);
+void Intro_Yo(char c[3][3]);
 void tablero(char c[3][3]);
 
 int main()
@@ -21,6 +22,8 @@ int main()
 void loop(char c[3][3])
 {
     Intro_Primera(c);
+    tablero(c);
+    Intro_Yo(c);
     tablero(c);
 }
 
@@ -38,6 +41,138 @@ void Intro_Primera(char c[3][3])
             c[i][j] = aux++;
         }
     }
+}
+
+void Intro_Yo(char c[3][3])
+{
+    int i, j, k;
+    char aux;
+
+    do
+    {
+        do
+        {
+            printf("Coloca una ficha: ");
+            fflush(stdin);
+            scanf("%c", &aux);
+        }
+        while (aux < '1' || aux > '9');
+
+        k = 0;
+        switch (aux)
+        {
+            case '1':
+                {
+                    i = 0;
+                    j = 0;
+                    if (c[i][j] == 'X' || c[i][j] == 'O')
+                    {
+                        k = 1;
+                        printf("La casilla está ocupada! Inténtalo con otro número!\n\n");
+                    }
+                    break;
+                }
+
+            case '2':
+                {
+                    i = 0;
+                    j = 1;
+                    if (c[i][j] == 'X' || c[i][j] == 'O')
+                    {
+                        k = 1;
+                        printf("La casilla está ocupada! Inténtalo con otro número!\n\n");
+                    }
+                    break;
+                }
+
+            case '3':
+                {
+                    i = 0;
+                    j = 2;
+                    if (c[i][j] == 'X' || c[i][j] == 'O')
+                    {
+                        k = 1;
+                        printf("La casilla está ocupada! Inténtalo con otro número!\n\n");
+                    }
+                    break;
+                }
+
+            case '4':
+                {
+                    i = 1;
+                    j = 0;
+                    if (c[i][j] == 'X' || c[i][j] == 'O')
+                    {
+                        k = 1;
+                        printf("La casilla está ocupada! Inténtalo con otro número!\n\n");
+                    }
+                    break;
+                }
+
+            case '5':
+                {
+                    i = 1;
+                    j = 1;
+                    if (c[i][j] == 'X' || c[i][j] == 'O')
+                    {
+                        k = 1;
+                        printf("La casilla está ocupada! Inténtalo con otro número!\n\n");
+                    }
+                    break;
+                }
+
+            case '6':
+                {
+                    i = 1;
+                    j = 2;
+                    if (c[i][j] == 'X' || c[i][j] == 'O')
+                    {
+                        k = 1;
+                        printf("La casilla está ocupada! Inténtalo con otro número!\n\n");
+                    }
+                    break;
+                }
+
+            case '7':
+                {
+                    i = 2;
+                    j = 0;
+                    if (c[i][j] == 'X' || c[i][j] == 'O')
+                    {
+                        k = 1;
+                        printf("La casilla está ocupada! Inténtalo con otro número!\n\n");
+                    }
+                    break;
+                }
+
+            case '8':
+                {
+                    i = 2;
+                    j = 1;
+                    if (c[i][j] == 'X' || c[i][j] == 'O')
+                    {
+                        k = 1;
+                        printf("La casilla está ocupada! Inténtalo con otro número!\n\n");
+                    }
+                    break;
+                }
+
+            case '9':
+                {
+                    i = 2;
+                    j = 2;
+                    if (c[i][j] == 'X' || c[i][j] == 'O')
+                    {
+                        k = 1;
+                        printf("La casilla está ocupada! Inténtalo con otro número!\n\n");
+                    }
+                    break;
+                }
+        }
+    }
+    while (k == 1);
+
+    c[i][j] = 'X';
 }
 
 void tablero(char c[3][3])
